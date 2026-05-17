84366183af16deeeb51d159d75a34ab625ab9e35
suleymanalizade75-pixel <suleyman.alizade75@gmail.com>
2026-05-17 22:17:05 +0400

    Remove unused import for QueryClientProvider

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 
-import { QueryClientProvider } from '@tanstack/react-query'
+
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
