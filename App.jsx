3feee1a8f0fa4f8bb065ce2ac9e4fa22e6b003ce
suleymanalizade75-pixel <suleyman.alizade75@gmail.com>
2026-06-19 23:29:22 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// страницы
+import Landing from './pages/Landing.jsx';
+import ProductDetail from './pages/ProductDetail.jsx';
+import Cart from './pages/Cart.jsx';
+import ProductsCatalog from './pages/ProductsCatalog.jsx';
+import ProductImport from './pages/ProductImport.jsx';
+import VaccineCompare from './pages/VaccineCompare.jsx';
+
+// вспомогательные
 import PageNotFound from './lib/PageNotFound.jsx';
-import { LangProvider } from './lib/i18n.jsx';
-import Landing from '@/pages/Landing.jsx';
-import ProductDetail from '@/pages/ProductDetail.jsx';
-import Cart from '@/pages/Cart.jsx';
-import ProductsCatalog from '@/pages/ProductsCatalog.jsx';
-import ProductImport from '@/pages/ProductImport.jsx';
-import VaccineCompare from '@/pages/VaccineCompare.jsx';
+import './i18n.jsx';
+
 
 const PublicApp = () => {
   return (
